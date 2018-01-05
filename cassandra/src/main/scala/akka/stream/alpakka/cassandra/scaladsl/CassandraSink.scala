891738f443110ad04b85149ec57167fcb6ffb706
Cedric CORBIERE <corbic@gmail.com>
2018-01-05 08:46:56 +0100

    WIP: Batching and change of signature

--- cassandra/src/main/scala/akka/stream/alpakka/cassandra/scaladsl/CassandraSink.scala
+++ cassandra/src/main/scala/akka/stream/alpakka/cassandra/scaladsl/CassandraSink.scala
@@ -6,19 +6,33 @@ package akka.stream.alpakka.cassandra.scaladsl
 
 import akka.Done
 import akka.stream.scaladsl.{Flow, Keep, Sink}
-import com.datastax.driver.core.{BoundStatement, PreparedStatement, Session}
+import com.datastax.driver.core._
 
 import scala.concurrent.Future
-
 import akka.stream.alpakka.cassandra.GuavaFutures._
 
 object CassandraSink {
   def apply[T](
       parallelism: Int,
       statement: PreparedStatement,
-      statementBinder: (T, PreparedStatement) => BoundStatement
+      statementBinder: (T, PreparedStatement) => Statement
   )(implicit session: Session): Sink[T, Future[Done]] =
     Flow[T]
       .mapAsyncUnordered(parallelism)(t ⇒ session.executeAsync(statementBinder(t, statement)).asScala())
       .toMat(Sink.ignore)(Keep.right)
+
+  def batch[T](
+      parallelism: Int,
+      statement: PreparedStatement,
+      statementBinder: (T, PreparedStatement) => Statement,
+      batchStatement: BatchStatement = new BatchStatement()
+  )(implicit session: Session): Sink[TraversableOnce[T], Future[Done]] = {
+    def batchElement(seq: TraversableOnce[T], ps: PreparedStatement): Statement =
+      seq.foldLeft(batchStatement) {
+        case (bs: BatchStatement, t: T) =>
+          bs.add(statementBinder(t, statement))
+      }
+    apply[TraversableOnce[T]](parallelism, statement, batchElement)
+  }
+
 }
